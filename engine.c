89567cb75c45050073b520fdf7dc1fb12a9feae1
jgcarrasco <jorgarcar97@gmail.com>
2024-01-29 11:42:27 +0100

    First ultra-basic working version of backprop engine

--- engine.c
+++ engine.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct value;
 typedef struct value value;
@@ -15,7 +16,14 @@ struct value {
     struct value** prev;
     /* Backprop function */
     backwardfun _backward;
+    /* Boolean used to traverse the graph */
+    bool visited;
 };
+
+typedef struct {
+    int count;
+    value** values;
+} topo;
 
 value* new_value(double data) {
     value* v = malloc(sizeof(value));
@@ -24,6 +32,7 @@ value* new_value(double data) {
     v->n_prev = 0;
     v->prev = NULL;
     v->_backward = NULL;
+    v->visited = false;
     return v;
 }
 
@@ -34,6 +43,50 @@ void free_value(value* v) {
     free(v->prev);
     free(v);
 }
+
+topo* new_topo() {
+    topo* t = malloc(sizeof(topo));
+    t->count = 0;
+    t->values = NULL;
+    return t;
+}
+
+void free_topo(topo* t) {
+    free(t->values);
+    free(t);
+}
+
+topo* topo_add(topo* t, value* v) {
+    t->count++;
+    t->values = realloc(t->values, t->count*sizeof(value*));
+    t->values[t->count-1] = v;
+    return t;
+}
+
+void _build_topo(topo*, value*);
+topo* build_topo(value* v) {
+    topo* t = new_topo();
+    _build_topo(t, v);
+    return t;
+}
+
+void _build_topo(topo* t, value* v) {
+    if (!v->visited) {
+        v->visited = true;
+        topo_add(t, v);
+        for (int i = 0; i < v->n_prev; i++) {
+            _build_topo(t, v->prev[i]);    
+        }
+    }
+}
+
+void print_topo(topo* t) {
+    for (int i = 0; i < t->count; i++) {
+        printf("(data=%.2f, grad=%.2f) ", t->values[i]->data, t->values[i]->grad);
+    }
+    printf("\n");
+}
+
 
 void print_value(value* v, char* name) {
     printf("%s(data=%.4f, grad=%.4f)\n", name, v->data, v->grad);
@@ -73,6 +126,15 @@ value* value_mul(value* u, value* v) {
     return out;
 }
 
+void backward(value* v) {
+    v->grad = 1.0;
+    topo* t = build_topo(v);
+    for (int i = 0; i < t->count; i++) {
+        if (t->values[i]->_backward) 
+            t->values[i]->_backward(t->values[i]);
+    }
+    free_topo(t);
+}
 
 int main(void) {
     value* x = new_value(-2);
@@ -80,13 +142,16 @@ int main(void) {
     value* z = new_value(-4);
     value* q = value_add(x, y);
     value* f = value_mul(q, z);
-    f->grad = 1.0;
-    f->_backward(f);
+
+    backward(f);
 
     print_value(f, "f");
     print_value(q, "q");
     print_value(z, "z");
-    free_value(f);
+    print_value(x, "x");
+    print_value(y, "y");
 
+
+    free_value(f);
     return 0;
 }
